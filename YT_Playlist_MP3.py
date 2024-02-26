e1dcc1008f0747a7f50c1e29da8ba3c29a967661
Wittusus <wittusus@gmail.com>
2024-02-27 00:27:50 +0200

    Added age-restriction skip instead of crash

--- YT_Playlist_MP3.py
+++ YT_Playlist_MP3.py
@@ -1,46 +1,52 @@
 import os, shutil, subprocess
 from tqdm import tqdm
 from pytube import Playlist, YouTube
+from pytube.exceptions import AgeRestrictedError
 
 def main(plist, destination_folder):
     links = plist.video_urls
     for link in tqdm(links, desc="Downloading"):
-        ytube = YouTube(link)
-        music = ytube.streams.filter(file_extension="mp4").first()
-        #filename of the first video stream
-        default_filename = music.default_filename
-        
-        # downloads first video stream and rename the first video stream
-        music.download()
-        default_filename_no_spaces = default_filename.replace(" ", "")
         try:
-            # if its already renamed then pass
-            os.rename(default_filename, default_filename_no_spaces)
-        except:
-            pass
+
+            ytube = YouTube(link)
+            music = ytube.streams.filter(file_extension="mp4").first()
+            #filename of the first video stream
+            default_filename = music.default_filename
             
-        # replaces mp4 with mp3 for ffmeg output
-        new_filename = default_filename.replace("mp4", "mp3")
-        new_filename_no_spaces = new_filename.replace(" ", "")
-        
-        # converts mp4 video to mp3 audio, needs either ffmpeg exe in dir or in system PATH
-        subprocess.call(f"ffmpeg -i {default_filename_no_spaces} {new_filename_no_spaces} -y", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        # if exception then create download folder if not exists and store the downloaded audios
-        # Move the converted audio file to the destination folder
-        # Move the converted audio file to the destination folder
-        new_audio_path = os.path.join(destination_folder, new_filename_no_spaces)
-        shutil.move(new_filename_no_spaces, new_audio_path)
+            # downloads first video stream and rename the first video stream
+            music.download()
+            default_filename_no_spaces = default_filename.replace(" ", "_").replace("(", "_").replace(")", "_").replace("&", "")
+            try:
+                # if its already renamed then pass
+                os.rename(default_filename, default_filename_no_spaces)
+            except:
+                pass
+                
+            # replaces mp4 with mp3 for ffmeg output
+            new_filename = default_filename_no_spaces.replace("mp4", "mp3")
+            new_filename_no_spaces = new_filename.replace(" ", "")
+            
+            # converts mp4 video to mp3 audio, needs either ffmpeg exe in dir or in system PATH
+            subprocess.call(f"ffmpeg -i {default_filename_no_spaces} {new_filename_no_spaces} -y", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # if exception then create download folder if not exists and store the downloaded audios
+            # Move the converted audio file to the destination folder
+            # Move the converted audio file to the destination folder
+            new_audio_path = os.path.join(destination_folder, new_filename_no_spaces)
+            shutil.move(new_filename_no_spaces, new_audio_path)
 
-        # Create "Downloaded music" directory if it doesn't exist in the destination folder
-        downloaded_music_dir = os.path.join(destination_folder, "Downloaded music")
-        if not os.path.exists(downloaded_music_dir):
-            os.makedirs(downloaded_music_dir)
+            # Create "Downloaded music" directory if it doesn't exist in the destination folder
+            downloaded_music_dir = os.path.join(destination_folder, "Downloaded music")
+            if not os.path.exists(downloaded_music_dir):
+                os.makedirs(downloaded_music_dir)
 
-        # Move the audio file to the "Downloaded music" directory
-        shutil.move(new_audio_path, os.path.join(downloaded_music_dir, new_filename_no_spaces))
+            # Move the audio file to the "Downloaded music" directory
+            shutil.move(new_audio_path, os.path.join(downloaded_music_dir, new_filename_no_spaces))
 
-        # Remove the downloaded video file
-        os.remove(default_filename_no_spaces)
+            # Remove the downloaded video file
+            os.remove(default_filename_no_spaces)
+        except AgeRestrictedError as e:
+            print(f"Skipped age-restricted video: {link}")
+            continue
 
                 
     print("Download finished.")
